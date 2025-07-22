16a1748cb3b226b0fab7ac53e72c81629ba29acb
wolf <douglaswolf@yahoo.com>
2025-07-22 12:59:46 -0700

    No change history kept

--- common/misc/axi4_lite_master.v
+++ common/misc/axi4_lite_master.v
@@ -10,8 +10,135 @@
 //====================================================================================
 // 09-Sep-22  DWW     1  Initial creation
 //
-// 02-May-22  DWW     2  Removed the un-needed AXI PROT signals
+// 02-May-22  DWW     2  Removed the un-needed AxPROT signals
+//
+// 21-Jul-22  DWW     3  Restored the wrongfully removed AxPROT signals!
+//                       Minor refactoring
 //====================================================================================
+
+
+/*
+        >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+        >>>> This is the AXI4 interface you should place in your module <<<<
+        <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+    //====================  An AXI-Lite Master Interface  ======================
+    // "Specify write address"          -- Master --    -- Slave --
+    output [AW-1:0]                     M_AXI_AWADDR,   
+    output                              M_AXI_AWVALID,  
+    output    [2:0]                     M_AXI_AWPROT,
+    input                                               M_AXI_AWREADY,
+
+    // "Write Data"                     -- Master --    -- Slave --
+    output [DW-1:0]                     M_AXI_WDATA,
+    output [DW/8-1:0]                   M_AXI_WSTRB,      
+    output                              M_AXI_WVALID,
+    input                                               M_AXI_WREADY,
+
+    // "Send Write Response"            -- Master --    -- Slave --
+    input  [1:0]                                        M_AXI_BRESP,
+    input                                               M_AXI_BVALID,
+    output                              M_AXI_BREADY,
+
+    // "Specify read address"           -- Master --    -- Slave --
+    output [AW-1:0]                     M_AXI_ARADDR,     
+    output [   2:0]                     M_AXI_ARPROT,
+    output                              M_AXI_ARVALID,
+    input                                               M_AXI_ARREADY,
+
+    // "Read data back to master"       -- Master --    -- Slave --
+    input [DW-1:0]                                      M_AXI_RDATA,
+    input                                               M_AXI_RVALID,
+    input [1:0]                                         M_AXI_RRESP,
+    output                              M_AXI_RREADY
+    //==========================================================================
+
+
+
+
+        >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+        >>>>              Declare these signals in your module          <<<<
+        <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+
+    //==================  The AXI Master Control Interface  ====================
+    // AMCI signals for performing AXI writes
+    reg [AW-1:0]  AMCI_WADDR;
+    reg [DW-1:0]  AMCI_WDATA;
+    reg           AMCI_WRITE;
+    wire[   1:0]  AMCI_WRESP;
+    wire          AMCI_WIDLE;
+
+    // AMCI signals for performing AXI reads
+    reg [AW-1:0]  AMCI_RADDR;
+    reg           AMCI_READ ;
+    wire[DW-1:0]  AMCI_RDATA;
+    wire[   1:0]  AMCI_RRESP;
+    wire          AMCI_RIDLE;
+    //==========================================================================
+
+
+
+
+        >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+        >>>>   Use the code below to instantiate the axi4_lite_master   <<<<
+        <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+
+
+    //==========================================================================
+    // This instantiates an AXI4-Lite master
+    //==========================================================================
+    axi4_lite_master # (.DW(DW), .AW(AW)) axi4_master
+    (
+        // Clock and reset
+        .clk            (clk),
+        .resetn         (resetn),
+
+        // AXI Master Control Interface for performing writes
+        .AMCI_WADDR     (AMCI_WADDR),
+        .AMCI_WDATA     (AMCI_WDATA),
+        .AMCI_WRITE     (AMCI_WRITE),
+        .AMCI_WRESP     (AMCI_WRESP),
+        .AMCI_WIDLE     (AMCI_WIDLE),
+
+        // AXI Master Control Interface for performing reads
+        .AMCI_RADDR     (AMCI_RADDR),
+        .AMCI_READ      (AMCI_READ ),
+        .AMCI_RDATA     (AMCI_RDATA),
+        .AMCI_RRESP     (AMCI_RRESP),
+        .AMCI_RIDLE     (AMCI_RIDLE),
+
+        // AXI4-Lite AW channel
+        .AXI_AWADDR     (M_AXI_AWADDR ),
+        .AXI_AWVALID    (M_AXI_AWVALID),
+        .AXI_AWPROT     (M_AXI_AWPROT ),
+        .AXI_AWREADY    (M_AXI_AWREADY),
+
+        // AXI4-Lite W channel
+        .AXI_WDATA      (M_AXI_WDATA  ),
+        .AXI_WSTRB      (M_AXI_WSTRB  ),
+        .AXI_WVALID     (M_AXI_WVALID ),
+        .AXI_WREADY     (M_AXI_WREADY ),
+
+        // AXI4-Lite B channel
+        .AXI_BRESP      (M_AXI_BRESP  ),
+        .AXI_BVALID     (M_AXI_BVALID ),
+        .AXI_BREADY     (M_AXI_BREADY ),
+
+        // AXI4-Lite AR channel
+        .AXI_ARADDR     (M_AXI_ARADDR ),
+        .AXI_ARPROT     (M_AXI_ARPROT ),
+        .AXI_ARVALID    (M_AXI_ARVALID),
+        .AXI_ARREADY    (M_AXI_ARREADY),
+
+        .AXI_RDATA      (M_AXI_RDATA  ),
+        .AXI_RVALID     (M_AXI_RVALID ),
+        .AXI_RRESP      (M_AXI_RRESP  ),
+        .AXI_RREADY     (M_AXI_RREADY )
+    );
+    //==========================================================================
+*/
 
 
 module axi4_lite_master#
@@ -21,7 +148,7 @@ module axi4_lite_master#
 )
 (
 
-    input wire clk, resetn,
+    input clk, resetn,
 
 
     //==================  The AXI Master Control Interface  ====================
@@ -39,8 +166,8 @@ module axi4_lite_master#
     output reg[DW-1:0]             AMCI_RDATA,
     output reg[1:0]                AMCI_RRESP,
     output                         AMCI_RIDLE,
-
     //==========================================================================
+
 
 
     //====================  An AXI-Lite Master Interface  ======================
@@ -48,21 +175,23 @@ module axi4_lite_master#
     // "Specify write address"          -- Master --    -- Slave --
     output reg [AW-1:0]                 AXI_AWADDR,   
     output reg                          AXI_AWVALID,  
+    output     [2:0]                    AXI_AWPROT,
     input                                               AXI_AWREADY,
 
     // "Write Data"                     -- Master --    -- Slave --
     output reg [DW-1:0]                 AXI_WDATA,
-    ouutput    [DW/8-1:0]               AXI_WSTRB,      
+    output     [DW/8-1:0]               AXI_WSTRB,      
     output reg                          AXI_WVALID,
     input                                               AXI_WREADY,
 
     // "Send Write Response"            -- Master --    -- Slave --
     input      [1:0]                                    AXI_BRESP,
     input                                               AXI_BVALID,
-    output reg                          AXI_BREADY,
+    output                              AXI_BREADY,
 
     // "Specify read address"           -- Master --    -- Slave --
     output reg [AW-1:0]                 AXI_ARADDR,     
+    output     [2:0]                    AXI_ARPROT,
     output reg                          AXI_ARVALID,
     input                                               AXI_ARREADY,
 
@@ -70,13 +199,21 @@ module axi4_lite_master#
     input [DW-1:0]                                      AXI_RDATA,
     input                                               AXI_RVALID,
     input [1:0]                                         AXI_RRESP,
-    output reg                          AXI_RREADY
+    output                              AXI_RREADY
     //==========================================================================
 
 );
   
     // All bytes of a write are always valid
     assign AXI_WSTRB = -1;
+
+    // These AxPROT signals are unused in virtually all cases
+    assign AXI_AWPROT = 0;
+    assign AXI_ARPROT = 0;
+
+    // BREADY and RREADY are asserted any time we're not in reset
+    assign AXI_BREADY = (resetn == 1);
+    assign AXI_RREADY = (resetn == 1);
 
     // Define the handshakes for all 5 AXI channels
     wire B_HANDSHAKE  = AXI_BVALID  & AXI_BREADY;
@@ -109,7 +246,6 @@ module axi4_lite_master#
             write_state <= 0;
             AXI_AWVALID <= 0;
             AXI_WVALID  <= 0;
-            AXI_BREADY  <= 0;
         end        
         
         // Otherwise, we're not in RESET and our state machine is running
@@ -123,7 +259,6 @@ module axi4_lite_master#
                     AXI_WDATA   <= AMCI_WDATA;  // Place our data onto the bus
                     AXI_AWVALID <= 1;           // Indicate that the address is valid
                     AXI_WVALID  <= 1;           // Indicate that the data is valid
-                    AXI_BREADY  <= 1;           // Indicate that we're ready for the slave to respond
                     write_state <= 1;           // On the next clock cycle, we'll be in the next state
                 end
                 
@@ -145,7 +280,6 @@ module axi4_lite_master#
            // Wait around for the slave to assert "AXI_BVALID".  
            2:   if (B_HANDSHAKE) begin
                     AMCI_WRESP  <= AXI_BRESP;
-                    AXI_BREADY  <= 0;
                     write_state <= 0;
                 end
 
@@ -172,7 +306,6 @@ module axi4_lite_master#
         if (resetn == 0) begin
             read_state  <= 0;
             AXI_ARVALID <= 0;
-            AXI_RREADY  <= 0;
         end else case (read_state)
 
             // Here we are waiting around for someone to raise "AMCI_READ", which signals us to begin
@@ -180,12 +313,10 @@ module axi4_lite_master#
             0:  if (AMCI_READ) begin
                     AXI_ARADDR   <= AMCI_RADDR; 
                     AXI_ARVALID  <= 1;
-                    AXI_RREADY   <= 1;
                     read_state   <= 1;
                 end else begin
                     AXI_ARADDR   <= 0;
                     AXI_ARVALID  <= 0;
-                    AXI_RREADY   <= 0;
                     read_state   <= 0;
                 end
             
@@ -204,9 +335,6 @@ module axi4_lite_master#
                         
                         // Save the read-response so our user can get it
                         AMCI_RRESP <= AXI_RRESP;
-                        
-                        // Lower the ready signal to make it obvious that we're done
-                        AXI_RREADY <= 0;
 
                         // And go back to idle state
                         read_state <= 0;
